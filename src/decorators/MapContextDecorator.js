d65741a2a03bfbc2e7e3b9a0bb5167d0aa08a99c
Max Tobias Weber <tobias.weber@wheregroup.com>
2021-12-04 15:55:14 +0100

    Add @mui ThemeProvider to MapContextDecorator

--- src/decorators/MapContextDecorator.js
+++ src/decorators/MapContextDecorator.js
@@ -2,23 +2,29 @@ import React from "react";
 
 import { MapComponentsProvider } from "@mapcomponents/react-core";
 import { MapLibreMap } from "@mapcomponents/react-maplibre";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 import "./style.css";
+
+const theme = createTheme({});
 
 const decorators = [
   (Story) => (
     <div className="fullscreen_map">
-      <MapComponentsProvider>
-        <Story />
+      <ThemeProvider theme={theme}>
+        <MapComponentsProvider>
+          <Story />
 
-        <MapLibreMap
-          options={{
-            zoom: 14.5,
-            style: "https://wms.wheregroup.com/tileserver/style/osm-bright.json",
-            center: [7.0851268, 50.73884],
-          }}
-          mapId="map_1"
-        />
-      </MapComponentsProvider>
+          <MapLibreMap
+            options={{
+              zoom: 14.5,
+              style: "https://wms.wheregroup.com/tileserver/style/osm-bright.json",
+              center: [7.0851268, 50.73884],
+            }}
+            mapId="map_1"
+          />
+        </MapComponentsProvider>
+      </ThemeProvider>
     </div>
   ),
 ];
