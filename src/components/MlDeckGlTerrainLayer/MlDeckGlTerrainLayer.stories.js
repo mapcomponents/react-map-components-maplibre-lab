4cdb8acc058e5ce40dbabd5169cff56b2cd4c72f
mludwig <marwin.ludwig@wheregroup.com>
2022-10-24 18:56:23 +0200

    optimize cameraFollow & Terrain Demo

--- src/components/MlDeckGlTerrainLayer/MlDeckGlTerrainLayer.stories.js
+++ src/components/MlDeckGlTerrainLayer/MlDeckGlTerrainLayer.stories.js
@@ -1,181 +1,192 @@
-import React, { useState } from "react";
-import MlCameraFollowPath from "../MlCameraFollowPath/MlCameraFollowPath";
-import TopToolbar from "../../ui_components/TopToolbar";
-import mapContextDecorator from "../../decorators/MapContextDecorator";
-import { Button, Slider, Typography } from "@mui/material";
-import { MlGeoJsonLayer, MlNavigationTools } from "@mapcomponents/react-maplibre";
+import React, { useState } from 'react';
+import MlCameraFollowPath from '../MlCameraFollowPath/MlCameraFollowPath';
+import TopToolbar from '../../ui_components/TopToolbar';
+import mapContextDecorator from '../../decorators/MapContextDecorator';
+import { Button, Slider, Typography } from '@mui/material';
+import MlGeoJsonLayer from '../MlGeoJsonLayer/MlGeoJsonLayer';
+import MlNavigationTools from '../MlNavigationTools/MlNavigationTools';
 
-import MlDeckGlTerrainLayer from "./MlDeckGlTerrainLayer";
+import MlDeckGlTerrainLayer from './MlDeckGlTerrainLayer';
 
 const storyoptions = {
-  title: "MapComponents/MlDeckGlTerrainLayer",
-  component: MlDeckGlTerrainLayer,
-  argTypes: {
-    options: {
-      control: {
-        type: "object",
-      },
-    },
-  },
-  decorators: mapContextDecorator,
+	title: 'MapComponents/MlDeckGlTerrainLayer',
+	component: MlDeckGlTerrainLayer,
+	argTypes: {
+		options: {
+			control: {
+				type: 'object',
+			},
+		},
+	},
+	decorators: mapContextDecorator,
 };
 export default storyoptions;
-//<MlFillExtrusionLayer />
 
 const routeJson = {
-  type: "Feature",
-  properties: {},
-  geometry: {
-    type: "LineString",
-    coordinates: [
-      [11.200688, 47.427417],
-      [9.874942, 47.215719],
-    ],
-  },
+	type: 'Feature',
+	properties: {},
+	geometry: {
+		type: 'LineString',
+		coordinates: [
+			[11.200688, 47.427417],
+			[9.874942, 47.215719],
+		],
+	},
 };
 
 const marks = [
-  {
-    value: 8,
-    label: "8",
-  },
-  {
-    value: 9,
-    label: "9",
-  },
-  {
-    value: 10,
-    label: "11",
-  },
-  {
-    value: 11,
-    label: "11",
-  },
-  {
-    value: 12,
-    label: "12",
-  },
-  {
-    value: 13,
-    label: "13",
-  },
+	{
+		value: 8,
+		label: '8',
+	},
+	{
+		value: 9,
+		label: '9',
+	},
+	{
+		value: 10,
+		label: '11',
+	},
+	{
+		value: 11,
+		label: '11',
+	},
+	{
+		value: 12,
+		label: '12',
+	},
+	{
+		value: 13,
+		label: '13',
+	},
 ];
 
 const Template = (args) => {
-  const [pause, setPause] = useState(false);
-  const [zoom, setZoom] = useState(11);
-  const [speed, setSpeed] = useState(10);
-  const [pitch, setPitch] = useState("3D");
-  const [disable, setDisable] = useState(false);
+	const [state, setState] = useState({ pause: true, zoom: 11, speed: 10, pitch: 60 });
 
-  const CameraFollowPath = MlCameraFollowPath({
-    route: routeJson,
-    pause: pause,
-    pitch: pitch,
-    zoom: zoom,
-    speed: speed,
-  });
+	const CameraFollowPath = MlCameraFollowPath({
+		route: routeJson,
+		pause: state.pause,
+		pitch: state.pitch,
+		zoom: state.zoom,
+		speed: state.speed,
+	});
 
-  function doPlay(event) {
-    setPause(false);
-    setTimeout(() => {
-      CameraFollowPath.play();
-    }, 10);
-    setDisable(true);
-  }
-  function doReset() {
-    setPause(true);
-    CameraFollowPath.reset();
-    setDisable(false);
-    setPitch("3D");
-    setZoom(11);
-    setSpeed(10);
-  }
-  function doPitch() {
-    if (pitch === "2D") {
-      setPitch("3D");
-    } else {
-      setPitch("2D");
-    }
-  }
-
-  return (
-    <>
-      <TopToolbar>
-        <MlDeckGlTerrainLayer />
-        <Button disabled={disable} onClick={doPlay}>
-          Start
-        </Button>
-        <Button
-          disabled={!disable}
-          onClick={() => (setPause(true), setDisable(false))}
-        >
-          Pause
-        </Button>
-        <Button onClick={doReset}>Reset</Button>
-        <Typography
-          id="discrete-slider"
-          style={{ color: "#121212", marginLeft: "10px", marginRight: "10px" }}
-        >
-          Zoom:
-        </Typography>
-        <Slider
-          value={zoom}
-          onChange={(ev, value) => {
-            setZoom(value);
-          }}
-          getAriaValueText={(value) => value}
-          aria-labelledby="discrete-slider"
-          //valueLabelDisplay="auto"
-          step={1}
-          marks={marks}
-          min={8}
-          max={13}
-          sx={{
-            marginTop: "20px",
-            paddingBottom: "20px",
-            marginRight: "10px",
-            maxWidth: "200px",
-          }}
-        />
-        <Typography
-          id="discrete-slider2"
-          style={{ color: "#121212", marginLeft: "10px", marginRight: "10px" }}
-        >
-          Speed:
-        </Typography>
-        <Slider
-          value={speed}
-          onChange={(ev, value) => {
-            setSpeed(value);
-          }}
-          getAriaValueText={(value) => value}
-          aria-labelledby="discrete-slider2"
-          //valueLabelDisplay="auto"
-          step={1}
-          marks
-          min={1}
-          max={20}
-          sx={{
-            marginRight: "10px",
-            maxWidth: "200px",
-          }}
-        />
-        <Button onClick={doPitch}>{pitch === "2D" ? "3D" : "2D"}</Button>
-      </TopToolbar>
-      <MlGeoJsonLayer
-        geojson={routeJson}
-        type="line"
-        paint={{
-          "line-width": 2,
-          "line-color": "blue",
-        }}
-      />
-      {/*
-      <MlNavigationTools />
-        */}
-    </>
-  );
+	return (
+		<>
+			<TopToolbar>
+				<MlDeckGlTerrainLayer />
+				<Button
+					disabled={!state.pause}
+					onClick={() =>
+						setState((current) => {
+							return { ...current, pause: false };
+						})
+					}
+				>
+					Start
+				</Button>
+				<Button
+					disabled={state.pause}
+					onClick={() =>
+						setState((current) => {
+							return { ...current, pause: true };
+						})
+					}
+				>
+					Pause
+				</Button>
+				<Button
+					onClick={() => {
+						CameraFollowPath.reset();
+						setState((current) => {
+							return { ...current, pause: true, pitch: 60, zoom: 18, speed: 1 };
+						});
+					}}
+				>
+					Reset
+				</Button>
+				<Typography
+					id="discrete-slider"
+					style={{ color: '#121212', marginLeft: '10px', marginRight: '10px' }}
+				>
+					Zoom:
+				</Typography>
+				<Slider
+					value={state.zoom}
+					onChange={(ev, value) => {
+						setState((current) => {
+							return { ...current, zoom: value };
+						});
+					}}
+					getAriaValueText={(value) => value}
+					aria-labelledby="discrete-slider"
+					//valueLabelDisplay="auto"
+					step={1}
+					marks={marks}
+					min={15}
+					max={20}
+					sx={{
+						marginTop: '20px',
+						paddingBottom: '20px',
+						marginRight: '10px',
+						maxWidth: '200px',
+					}}
+				/>
+				<Typography
+					id="discrete-slider2"
+					style={{ color: '#121212', marginLeft: '10px', marginRight: '10px' }}
+				>
+					Speed:
+				</Typography>
+				<Slider
+					value={state.speed}
+					onChange={(ev, value) => {
+						setState((current) => {
+							return { ...current, speed: value };
+						});
+					}}
+					getAriaValueText={(value) => value}
+					aria-labelledby="discrete-slider2"
+					//valueLabelDisplay="auto"
+					step={0.1}
+					marks
+					min={0.1}
+					max={2}
+					sx={{
+						marginRight: '10px',
+						maxWidth: '200px',
+					}}
+				/>
+				<Button
+					onClick={() => {
+						if (state.pitch === 0) {
+							setState((current) => {
+								return { ...current, pitch: 60 };
+							});
+						} else {
+							setState((current) => {
+								return { ...current, pitch: 0 };
+							});
+						}
+					}}
+				>
+					{state.pitch === 0 ? '3D' : '2D'}
+				</Button>
+			</TopToolbar>
+			<MlGeoJsonLayer
+				geojson={routeJson}
+				type="line"
+				paint={{
+					'line-width': 2,
+					'line-color': 'blue',
+				}}
+			/>
+			{/*
+      		<MlNavigationTools />
+       	 	*/}
+		</>
+	);
 };
 
 export const ExampleConfig = Template.bind({});
