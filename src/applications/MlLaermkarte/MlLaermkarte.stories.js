c5972c1c263d06313ac7d676a7e27dc3e2ccba8b
mludwig <marwin.ludwig@wheregroup.com>
2022-11-16 18:38:17 +0100

    MlLaermkarte use now useCameraFollowPath

--- src/applications/MlLaermkarte/MlLaermkarte.stories.js
+++ src/applications/MlLaermkarte/MlLaermkarte.stories.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 
 import MlLaermkarte from "./MlLaermkarte";
 import { MlFillExtrusionLayer } from "@mapcomponents/react-maplibre";
 import MlCameraFollowPath from "../../components/MlCameraFollowPath/MlCameraFollowPath";
-import { MapContext, SimpleDataProvider } from "@mapcomponents/react-maplibre";
+import { useMap, SimpleDataProvider, useCameraFollowPath } from "@mapcomponents/react-maplibre";
 import DeckGlProvider from "../../deckgl_components/DeckGlProvider";
 
 //import mapContext3DDecorator from "../../decorators/MapContext3DDecorator";
@@ -20,44 +20,53 @@ const storyoptions = {
 };
 export default storyoptions;
 
-const route = [
-  //[7.09222, 50.725055],
-  //[7.1579, 50.681],
-  //[7.0577, 50.7621],
-  [7.10942788610961, 50.708209240168],
-  [7.10966149846967, 50.7088867160122],
-  [7.10910082880551, 50.7108256986007],
-  [7.10856352037736, 50.7126945974813],
-  [7.1083532692533, 50.7142598002937],
-  [7.10814301812924, 50.7160118929942],
-  [7.10793276700518, 50.7169463424345],
-  [7.10776923835314, 50.7176004570426],
-  [7.10713848498096, 50.718838602551],
-  [7.10699831756492, 50.7199599418793],
-  [7.106900786313568, 50.72118132611057],
-];
+const routeJson = {
+  type: "Feature",
+  properties: {},
+  geometry: {
+    type: "LineString",
+    coordinates: [
+      [7.10942788610961, 50.708209240168],
+      [7.10966149846967, 50.7088867160122],
+      [7.10910082880551, 50.7108256986007],
+      [7.10856352037736, 50.7126945974813],
+      [7.1083532692533, 50.7142598002937],
+      [7.10814301812924, 50.7160118929942],
+      [7.10793276700518, 50.7169463424345],
+      [7.10776923835314, 50.7176004570426],
+      [7.10713848498096, 50.718838602551],
+      [7.10699831756492, 50.7199599418793],
+      [7.106900786313568, 50.72118132611057],
+    ],
+  },
+};
 
 const Template = (args) => {
-  const mapContext = useContext(MapContext);
+  const mapHook = useMap({mapId: "map_1"});
+
+  const CameraFollowPath = useCameraFollowPath({
+    route: routeJson,
+    pause: false,
+    pitch: 60,
+    zoom: 15,
+    speed: 1,
+  });
 
   useEffect(() => {
-    if (!mapContext?.mapExists()) return;
+    if (!mapHook.map) return;
+    CameraFollowPath.play();
+  }, [mapHook.map]);
 
-    mapContext.map.setCenter(route[0]);
-    mapContext.map.setPitch(60);
-  }, [mapContext.map]);
+  useEffect(() => {
+    if (!mapHook) return;
+    CameraFollowPath.play();
+  }, [mapHook]);
 
   return (
     <>
       <DeckGlProvider>
         <SimpleDataProvider format="json" url="assets/laerm_points.json">
           <MlLaermkarte />
-          <MlCameraFollowPath
-            path={route}
-            initialZoom={15.8}
-            zoomOutTo={13.6}
-            kmPerStep={0.008}
-          ></MlCameraFollowPath>
         </SimpleDataProvider>
       </DeckGlProvider>
     </>
